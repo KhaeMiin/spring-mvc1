89bdc2b85c5ffd7c10b6990d80b1afa56905fe39
minMin <goals1005@naver.com>
2022-03-26 22:48:07 +0900

    handlerAdapter 유연한 컨트롤러1 V5

---
+++ src/main/java/hello/servlet/web/forntController/v5/adapter/ControllerV3HandlerAdapter.java
@@ -0,0 +1,37 @@
+package hello.servlet.web.forntController.v5.adapter;
+
+import hello.servlet.web.forntController.ModelView;
+import hello.servlet.web.forntController.v3.ControllerV3;
+import hello.servlet.web.forntController.v5.MyHandlerAdapter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
+
+    @Override
+    public boolean supports(Object handler) {
+        return (handler instanceof ControllerV3);//ControllerV3의 인스턴스인지?
+    }
+
+    @Override
+    public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
+        ControllerV3 controller = (ControllerV3) handler;//ControllerV3로 핸들러 변환
+
+        Map<String, String> paramMap = createParamMap(request);
+        ModelView mv = controller.process(paramMap);
+
+        return mv;
+    }
+
+    private Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+        return paramMap;
+    }
+}
